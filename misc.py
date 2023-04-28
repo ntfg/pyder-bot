74870b2b4cada38690e6d005db380cd738da2241
ntfg <72043165+ntfg@users.noreply.github.com>
2023-04-28 14:28:41 +0300

    redis

--- misc.py
+++ misc.py
@@ -3,13 +3,13 @@ import aioredis
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.dispatcher import FSMContext
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 load_dotenv()
 token = os.getenv("BOT_TOKEN")
 
 redis = aioredis.Redis()
+storage= RedisStorage2()
 
 bot = Bot(token=token)
-dp = Dispatcher(bot, storage=MemoryStorage())
+dp = Dispatcher(bot, storage=storage)
